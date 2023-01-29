c972881b6dd857b4d444ad4feded3d6c5ca4692c
Daanjs <dan.alphadev@gmail.com>
2023-01-29 09:36:53 +0700

    create instalation guide

--- src/core/router.ts
+++ src/core/router.ts
@@ -1,7 +1,24 @@
 import { Router } from "express";
 import { IRouters } from "../interfaces";
+import { Authorization } from "../utils";
 
 abstract class Routers implements IRouters {
+  public router: Router;
+  constructor() {
+    this.router = Router();
+    this.router.use(Authorization);
+    this.getMethods();
+    this.postMethods();
+    this.putMethods();
+    this.deleteMethods();
+  }
+  abstract getMethods(): void;
+  abstract postMethods(): void;
+  abstract putMethods(): void;
+  abstract deleteMethods(): void;
+}
+
+export abstract class AuthRouters implements IRouters {
   public router: Router;
   constructor() {
     this.router = Router();
--- src/features/categories/categoryRouter.ts
+++ src/features/categories/categoryRouter.ts
@@ -1,25 +1,12 @@
-import { Router } from "express";
-import { IRouters } from "../../interfaces";
-import { Authorization } from "../../utils";
+import { Routers } from "../../core";
 import CategoryControllers from "./categoryControllers";
 
-class CategoryRouter implements IRouters {
-  public router: Router;
-  constructor() {
-    this.router = Router();
-    this.getMethods();
-    this.postMethods();
-    this.putMethods();
-    this.deleteMethods();
-  }
-
+class CategoryRouter extends Routers {
   getMethods(): void {
-    this.router.use(Authorization);
     this.router.get("/", CategoryControllers.getAllCategories);
     this.router.get("/find", CategoryControllers.getCategoryById);
   }
   postMethods(): void {
-    this.router.use(Authorization);
     this.router.post("/create", CategoryControllers.createNewCategory);
   }
   putMethods(): void {
